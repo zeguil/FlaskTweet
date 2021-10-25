0a7e32c7aea8284950fa8350ea8006327795198d
zeguil <zeguilhermelins@hotmail.com>
2021-10-25 10:18:25 -0400

    config

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///storage.db"
+app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+
 
 from .controllers import default
 
 #atualização
+
